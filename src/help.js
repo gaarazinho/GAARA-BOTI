a79ef5fceb54904d9416170d1cc9019321cca605
gaarazinho <76838261+gaarazinho@users.noreply.github.com>
2021-01-04 23:28:09 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,93 @@
+const help = (prefix) => {
+	❉────────────❉ 
+
+            モℜモղ  ɮロで ツ 
+
+❉────────────❉ 
+> *Sticker Commands* <
+command : *.sticker* or *.stiker*
+desc : converter imagem / gif / vídeo em adesivo
+usage : responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda
+───────────
+command : *.sticker nobg* or *.stiker nobg*
+desc : converter imagem em adesivo removendo o fundo
+usage : reply image, or send image with caption
+───────────
+command : *.toimg*
+desc : converter adesivo em imagem
+usage : reply sticker
+───────────
+command : *.tsticker* or *.tstiker*
+desc : converter texto em adesivo
+usage : *.tsticker text in here*
+───────────
+> *Meme Commands* <
+command : *.meme*
+desc : imagens aleatórias de meme [english]
+usage : just send the command
+───────────
+command : *.memeindo*
+desc : imagens aleatórias de meme [indo]
+usage : apenas envie o comando
+───────────
+> *Others Commands* <
+command : *.gtts*
+desc : converter texto em fala / áudio
+usage : *.gtts [cc] [text]*
+exemplo : *.gtts ja On2-chan*
+───────────
+command : *.loli*
+desc : imagens aleatórias de loli
+usage : apenas envie o comando
+───────────
+command : *.nsfwloli*
+desc : imagens aleatórias de nsfw loli
+usage: basta enviar o comando
+───────────
+command : *.url2img*
+desc : take web screenshots
+usage : *.url2img [tipe] [url]*
+───────────
+command : *.simi*
+desc : sua mensagem será respondida por simi
+usage : *.simi yourmessage*
+───────────
+command : *.ocr*
+desc : pegue o texto na foto
+usage : responder imagem ou enviar imagem com legenda
+───────────
+command : *.wait*
+desc : search anime with image [ What Anime Is This/That ]
+usage : responder imagem ou enviar imagem com legendan
+───────────
+command : *.setprefix*
+desc : substituir prefixo
+usage : *.setprefix [text|optional]*
+example : *.setprefix ?*
+note : This command can only be used by the bot owner
+───────────
+> *Group Comands* <
+command : *.linkgroup*
+desc : pegue o link do grupo
+usage : just send the commandnote : can only be used when the bot becomes admin, and the one who sends the command is admin!
+───────────
+command : *.tagall*
+desc : marca todos os membros do grupo, incluindo administradores também
+usage : apenas envie o comando
+note : Este comando pode ser usado se você for um administrador do grupo
+───────────
+command : *.simih*
+desc : ative o modo simi no grupo
+usage : *.simih 1* to activate simi mode and *.simih 0* para desativar o modo simi
+note : Este comando pode ser usado se você for um admin ╠═══════════════════
+║        ᏀᎪᎪᏒᎪ O ᏴᎡᎪᏴᎾ 😳🤙
+╚═══════════════════
+
+
+
+
+
+
+}
+
+exports.help = help
